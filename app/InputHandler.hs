7604ca76b19a6302d2fbca4fe86585f0917a1a78
igor-raffael <igor.raffael.menezes.melo@ccc.ufcg.edu.br>
2025-03-02 19:02:06 -0300

    Add arrow key support for cursor movement in terminal
    Implemented arrow key handling to allow cursor movement in the terminal.

--- app/InputHandler.hs
+++ app/InputHandler.hs
@@ -6,19 +6,32 @@ module InputHandler where
 import Editor.CommandMode (handleCommandMode)
 import Editor.Cursor
 import Editor.EditorState
+import Editor.ExtendedPieceTable
 import Editor.InsertMode
 import Editor.NormalMode
 
 handleKeyPress :: EditorState -> [Char] -> IO EditorState
-handleKeyPress state key = do
-  let (_, _, _, _, _, lineSizes) = extendedPieceTable state
-      isInsertMode = mode state == Insert
-  case key of
-    "\ESC[A" -> return state {cursor = updateCursor 'k' (cursor state) lineSizes isInsertMode}
-    "\ESC[B" -> return state {cursor = updateCursor 'j' (cursor state) lineSizes isInsertMode}
-    "\ESC[C" -> return state {cursor = updateCursor 'l' (cursor state) lineSizes isInsertMode}
-    "\ESC[D" -> return state {cursor = updateCursor 'h' (cursor state) lineSizes isInsertMode}
-    _ -> case mode state of
-      Normal -> handleNormalMode state key
-      Insert -> handleInsertMode state key
-      Command -> handleCommandMode state key
+handleKeyPress state inputChar
+  | mode state == Insert && inputChar `elem` ["\ESC[A", "\ESC[B", "\ESC[C", "\ESC[D"] = do
+      let direction = case inputChar of
+            "\ESC[A" -> 'k'
+            "\ESC[B" -> 'j'
+            "\ESC[C" -> 'l'
+            "\ESC[D" -> 'h'
+          (pieces, originalBuffer, addBuffer, insertBuffer, _, lineSizes) = insertText (extendedPieceTable state)
+          newCursor = updateCursor direction (cursor state) lineSizes True
+          newInsertStartIndex = cursorXYToStringIndex newCursor lineSizes 0 0
+          newExtendedPieceTable = (pieces, originalBuffer, addBuffer, insertBuffer, newInsertStartIndex, lineSizes)
+      return state {cursor = newCursor, extendedPieceTable = newExtendedPieceTable}
+  | mode state == Normal && inputChar `elem` ["\ESC[A", "\ESC[B", "\ESC[C", "\ESC[D"] = do
+      let direction = case inputChar of
+            "\ESC[A" -> 'k'
+            "\ESC[B" -> 'j'
+            "\ESC[C" -> 'l'
+            "\ESC[D" -> 'h'
+          (_, _, _, _, _, lineSizes) = extendedPieceTable state
+      return state {cursor = updateCursor direction (cursor state) lineSizes False}
+  | otherwise = case mode state of
+      Normal -> handleNormalMode state inputChar
+      Insert -> handleInsertMode state inputChar
+      Command -> handleCommandMode state inputChar
