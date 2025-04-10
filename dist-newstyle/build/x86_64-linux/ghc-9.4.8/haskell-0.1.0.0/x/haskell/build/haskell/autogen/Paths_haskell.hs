508d60b05ae1451b8a2a198657622025817e1ab3
LazaroQueiroz <lazaro.queiroz.nascimento@ccc.ufcg.edu.br>
2025-04-10 14:07:25 -0300

    fix: fixing line breaks when opening a file

--- dist-newstyle/build/x86_64-linux/ghc-9.4.8/haskell-0.1.0.0/x/haskell/build/haskell/autogen/Paths_haskell.hs
+++ dist-newstyle/build/x86_64-linux/ghc-9.4.8/haskell-0.1.0.0/x/haskell/build/haskell/autogen/Paths_haskell.hs
@@ -46,12 +46,12 @@ getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: 
 
 
 bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
-bindir     = "/home/igor-raffael/.cabal/bin"
-libdir     = "/home/igor-raffael/.cabal/lib/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0-inplace-haskell"
-dynlibdir  = "/home/igor-raffael/.cabal/lib/x86_64-linux-ghc-9.4.8"
-datadir    = "/home/igor-raffael/.cabal/share/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0"
-libexecdir = "/home/igor-raffael/.cabal/libexec/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0"
-sysconfdir = "/home/igor-raffael/.cabal/etc"
+bindir     = "/home/lazaro/.cabal/bin"
+libdir     = "/home/lazaro/.cabal/lib/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0-inplace-haskell"
+dynlibdir  = "/home/lazaro/.cabal/lib/x86_64-linux-ghc-9.4.8"
+datadir    = "/home/lazaro/.cabal/share/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0"
+libexecdir = "/home/lazaro/.cabal/libexec/x86_64-linux-ghc-9.4.8/haskell-0.1.0.0"
+sysconfdir = "/home/lazaro/.cabal/etc"
 
 getBinDir     = catchIO (getEnv "haskell_bindir")     (\_ -> return bindir)
 getLibDir     = catchIO (getEnv "haskell_libdir")     (\_ -> return libdir)
